e80889a90e455cdb6b15c9f351bc5b9000a4e3d0
Hanson <hanson.hua903@gmail.com>
2023-09-14 19:52:49 -0500

    Update Westworld Logo

--- src/services/Images.js
+++ src/services/Images.js
@@ -1,1 +1,1 @@
-export const westworldLogo = "https://www.hbo.com/content/dam/hbodata/series/westworld/episodes/s-01/westworld-s1-1920x1080.jpg"
+export const westworldLogo = "https://wme-gep-drupal-hbo-prod.s3.amazonaws.com/content/dam/hbodata/series/westworld/episodes/s-01/westworld-s1-1920x1080.jpg"
